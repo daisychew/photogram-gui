f2ed31dd9bb102d00534498f968e765c06e89e89
Daisy Chew <150085367+daisychew@users.noreply.github.com>
2024-02-16 21:09:01 +0000

    added controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get("/users", {:controller => "user", action: => "index" })
 
+  post("/users/:username", {:controller => "user", :action => "show"})
+
+  get("/photos", {:controller => "photo", action: => "index" })
+
+  post("/photos/:photo_id", {:controller => "photo", :action => "show"})
 end
