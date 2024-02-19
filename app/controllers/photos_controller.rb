ee9f45db257837779fc58f74b224c67583b9e95d
Daisy Chew <150085367+daisychew@users.noreply.github.com>
2024-02-19 19:02:19 +0000

    completed photos show

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -16,4 +16,44 @@ class PhotosController < ApplicationController
 
     render(template: "photo_templates/show")
   end
+
+  def delete
+    photo_id = params.fetch("photo_id")
+
+    @the_photo = Photo.find(photo_id)
+    
+    @the_photo.destroy
+
+    redirect_to("/photos", { :notice => "Photo deleted successfully."})
+  end
+
+  def update
+    photo_id = params.fetch("photo_id")
+    @the_photo = Photo.find(photo_id)
+
+    @the_photo.image = params.fetch("input_image")
+    @the_photo.caption = params.fetch("input_caption")
+
+    if @the_photo.valid?
+      @the_photo.save
+      redirect_to("/photos/#{@the_photo.id}", { :notice => "Photo updated successfully."} )
+    else
+      redirect_to("/photos/#{@the_photo.id}", { :alert => "Photo failed to update successfully." })
+    end
+  end
+
+  def create
+    @the_comment = Comment.new
+  
+    @the_comment.photo_id = params.fetch("input_photo_id")
+    @the_comment.author_id = params.fetch("input_author_id")
+    @the_comment.body = params.fetch("input_comment")
+  
+    if @the_comment.valid?
+      @the_comment.save
+      redirect_to("/photos/#{@the_comment.photo_id}", { :notice => "Photo created successfully." })
+    else
+      redirect_to("/photos/#{@the_comment.photo_id}", { :notice => "Photo failed to create successfully." })
+    end
+  end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,11 @@ Rails.application.routes.draw do
   get("/photos", {:controller => "photos", :action => "index" })
 
   get("/photos/:photo_id", {:controller => "photos", :action => "show"})
+
+  get("/delete_photo/:photo_id", {:controller => 
+"photos", :action => "delete"})
+
+  post("/update_photo/:photo_id", {:controller => "photos", :action => "update"})
+
+  post("/add_comment", {:controller => "photos", :action => "create"})
 end
