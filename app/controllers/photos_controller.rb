824637d44dc188271f94f42a8bd2623ef10e3ea6
Daisy Chew <150085367+daisychew@users.noreply.github.com>
2024-02-19 18:39:10 +0000

    finished photo show

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -8,7 +8,11 @@ class PhotosController < ApplicationController
   def show
     photo_id = params.fetch("photo_id")
 
-    @photo = Photo.where({:id => photo_id})
+    @photo = Photo.where({:id => photo_id}).at(0)
+
+    @photo_owner = User.where(:id => @photo.owner_id).at(0)
+
+    @comments = Comment.where({:photo_id => photo_id})
 
     render(template: "photo_templates/show")
   end
